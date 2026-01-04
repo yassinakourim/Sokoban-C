05669b265cbf6db32967fcc7a47af0e8ba092f50
YASSINE AKOURIM <yassinakourim@gmail.com>
2026-01-04 22:22:01 +0100

    Delete src directory

--- src/crate.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "entity.h"
-#include <SDL3/SDL.h>
-
-// This allows main.c to create crates
-Entity init_crate(SDL_Renderer* renderer, float x, float y);
-
-// This allows player.c to check if it can push a crate
-bool try_push_crate(float target_x, float target_y, float dx, float dy);
--- src/player.h
+++
@@ -1,7 +0,0 @@
-#pragma once 
-
-#include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-#include "entity.h"
-
-Entity init_player(SDL_Renderer* renderer);
